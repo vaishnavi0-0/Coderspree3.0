946408c58ce681c0bf0ba009507c1b9e274a7bae
AdityyaX <adityapachauri182003@gmail.com>
2023-10-01 15:10:11 +0530

    unnecessary file removed

--- Android/ayush27prasad_AyushPrasad_2125en1011_2/GettingStarted/PrintZ.java
+++
@@ -1,8 +0,0 @@
-public class PrintZ {
-
-  public static void main (String[] args) {
-
-    System.out.println("*****\n   * \n  *  \n *   \n*****");
-    
-  }
-}
--- IOT/Captain2234_Bhavishya_2125it1064_2/GettingStarted/competitive.cpp
+++
@@ -1,9 +0,0 @@
-# include<iostream>
-using namespace.std ::
-
-int main(){
-
-cout<<codespree;
-return 0;
-
-}
