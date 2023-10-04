0065328dd12f0de925ff8c01350346bc18d1d37d
AdityyaX <adityapachauri182003@gmail.com>
2023-10-04 19:00:02 +0530

    github actions added

--- check_submissions.py
+++ check_submissions.py
@@ -74,9 +74,9 @@ def check_structure(path, student: Student):
 
 def write_to_readme(filename, students_list):
 
-    mdFile = MdUtils(file_name=filename, title="Welcome to Coderspree2.0 🔥")
+    mdFile = MdUtils(file_name=filename, title="Welcome to Coderspree3.0 🎃")
 #     ![Innogeeks Logo](https://user-images.githubusercontent.com/33064931/193105033-d03aa2e9-d4c3-4d02-b33e-466f3c567f39.png)
-    mdFile.new_paragraph("## **This is a club only repo, limited only to members of Innogeeks.**")
+    mdFile.new_paragraph("## **This is a coding event repo.**")
     mdFile.new_line("<p align = 'center'><a href='https://innogeeks.in/' target='_blank'><img src='https://user-images.githubusercontent.com/33064931/193105033-d03aa2e9-d4c3-4d02-b33e-466f3c567f39.png' href='www.innogeeks.com'></a></p>")
 
     mdFile.new_paragraph(
@@ -140,7 +140,7 @@ def write_to_readme(filename, students_list):
 
 def write_to_pendingReadme(filename, students_list):
 
-    mdFile = MdUtils(file_name=filename, title="Welcome to Coderspree2.0 🔥")
+    mdFile = MdUtils(file_name=filename, title="Welcome to Coderspree3.0 🔥")
 
     list_of_strings = ["Profile", "Name", "Domain", "Solved", "Year", "logs"]
 
