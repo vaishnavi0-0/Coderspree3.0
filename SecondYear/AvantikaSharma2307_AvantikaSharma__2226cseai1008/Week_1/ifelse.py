e4c2017a8941f72578239b5d160ed65005ca244b
AvantikaSharma2307 <123316093+AvantikaSharma2307@users.noreply.github.com>
2023-10-07 22:46:57 +0530

    7problem completed

---
+++ SecondYear/AvantikaSharma2307_AvantikaSharma__2226cseai1008/Week_1/ifelse.py
@@ -0,0 +1,9 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+a=input()
+if(a.isupper()):
+    print(1)
+elif(a.islower()):
+    print(0)
+else:
+    print(-1)
