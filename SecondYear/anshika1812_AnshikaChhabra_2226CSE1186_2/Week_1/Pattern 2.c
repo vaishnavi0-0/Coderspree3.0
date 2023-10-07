5fe3a9894dc514db3ab79fe905d62d242de7ef84
anshika1812 <anshika.chhabra.dps@gmail.com>
2023-10-07 20:34:29 +0530

    Create Pattern 2.c

---
+++ SecondYear/anshika1812_AnshikaChhabra_2226CSE1186_2/Week_1/Pattern 2.c
@@ -0,0 +1,17 @@
+// My first contribution :)
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+    printf("Enter no. of rows: ");
+    scanf("%d",&n);
+    for(i=0;i<5;i++){
+        for(j=0;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
