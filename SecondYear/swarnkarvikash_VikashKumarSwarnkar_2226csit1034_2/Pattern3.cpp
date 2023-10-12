1fdc553896e7d191f197e43d1f05370d32c921f5
swarnkarvikash <vikashswarnkar3@gmail.com>
2023-10-12 23:29:52 +0530

    Update Pattern3.cpp

--- SecondYear/swarnkarvikash_VikashKumarSwarnkar_2226csit1034_2/Pattern3.cpp
+++ SecondYear/swarnkarvikash_VikashKumarSwarnkar_2226csit1034_2/Pattern3.cpp
@@ -1,20 +1,12 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-	// Write your code here
-	int n,n1=0,n2=0;
-	cin>>n;
-	while(n!=0){
-		int r=n%10;
-		
-		if(r%2==0){
-			 n1=n1+r;
-		}
-		else {
-			 n2=n2+r;
-		}
-		n=n/10;
-	}
-	cout<<n1<<" "<<n2;
-}
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
