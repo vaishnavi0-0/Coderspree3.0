36e06e84b713843d441a1fb24051e13c74515812
Aryan Rastogi <134607843+Arystar01@users.noreply.github.com>
2023-10-22 12:20:15 +0530

    Moving of all negative at end , Finish

---
+++ SecondYear/Arystar01_AryanRastogi_2226cse1169_2/Week_3/Move_all_negative_atend.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+       
+        printf("%d ",arr[i]);
+    }
+}
+int main(){
+    int arr[10],n,item;
+    printf("Enter the number of terms: \n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf("Enter the number:");
+        scanf("%d",&arr[i]);
+    }
+    printArray(arr,n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]<0 )
+            {int temp;
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    printf("The array after putting all negative as end is :");
+    printArray(arr,n);
+    return 0;
+}
